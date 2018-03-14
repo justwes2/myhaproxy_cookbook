95f1c0ee8529a2fd84a4546a3015f4245e23b40b
Wes <wes.coffay@gmail.com>
2018-03-14 10:41:32 -0400

    added second server to lb

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,17 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+
+node.default['haproxy']['members'] = [{
+    "hostname" => "ec2-35-171-163-19.compute-1.amazonaws.com",
+    "ipaddress" => "35.171.163.19",
+    "port" => 80,
+    "ssl_port" => 80
+  },{
+    "hostname" => "ec2-52-202-234-230.compute-1.amazonaws.com",
+    "ipaddress" => "52.202.234.230",
+    "port" => 80,
+    "ssl_port" => 80
+    }]
+
+include_recipe 'haproxy::manual'
